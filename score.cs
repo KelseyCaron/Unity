70736b60e5020943c84da4ff2d11dc41aaade81a
KelseyCaron <kelsey.jeh@gmail.com>
2019-02-03 22:34:13 -0600

    Create score.cs

---
+++ score.cs
@@ -0,0 +1,14 @@
+// Code From: Brackeys Youtube: https://www.youtube.com/channel/UCYbK_tjZ2OrIZFBvU6CCMiA
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public Transform player;
+    public Text scoreText;
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
