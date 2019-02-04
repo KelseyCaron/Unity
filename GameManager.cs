83f333a644161aadf29963912d4b01825e781d85
KelseyCaron <kelsey.jeh@gmail.com>
2019-02-03 22:33:31 -0600

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,26 @@
+// Code From: Brackeys Youtube: https://www.youtube.com/channel/UCYbK_tjZ2OrIZFBvU6CCMiA
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+
+    public void GameOver()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
